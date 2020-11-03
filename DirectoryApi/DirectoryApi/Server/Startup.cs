240561a6d376a8c2e9e24076272785b82bf2009b
Cory Maass <cory.maass@genmills.com>
2020-11-03 12:17:35 -0600

    Changed db connection to use GCP Secret Manager

--- DirectoryApi/DirectoryApi/Server/Startup.cs
+++ DirectoryApi/DirectoryApi/Server/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 
+using Google.Cloud.SecretManager.V1;
+using Npgsql;
+
 using DirectoryApi.DataAccess;
 
 namespace DirectoryApi.Server
@@ -24,14 +27,38 @@ namespace DirectoryApi.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            var connString = Configuration.GetConnectionString("DefaultConnection");
+            //var connString = Configuration.GetConnectionString("DefaultConnection");
 
-            if (String.IsNullOrWhiteSpace(connString))
-                connString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+            //if (String.IsNullOrWhiteSpace(connString))
+            //    connString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+            // Equivalent connection string: 
+            // "Server=<dbSocketDir>/<INSTANCE_CONNECTION_NAME>;Uid=<DB_USER>;Pwd=<DB_PASS>;Database=<DB_NAME>"
+            string dbSocketDir = Environment.GetEnvironmentVariable("DB_SOCKET_PATH") ?? "/cloudsql";
+            string instanceConnectionName = "dev-office-294516:us-central1:cory-sql";
+
+            var connectionString = new NpgsqlConnectionStringBuilder()
+            {
+                // The Cloud SQL proxy provides encryption between the proxy and instance. 
+                SslMode = SslMode.Disable,
+
+                // Remember - storing secrets in plaintext is potentially unsafe. Consider using
+                // something like https://cloud.google.com/secret-manager/docs/overview to help keep
+                // secrets secret.
+                Host = string.Format("{0}/{1}", dbSocketDir, instanceConnectionName),
+                Username = GetDBUserName(),
+                Password = GetDBPassword(),
+                Database = "postgres"
+
+            };
+            connectionString.Pooling = true;
+
+            NpgsqlConnection connection =
+                new NpgsqlConnection(connectionString.ConnectionString);
 
             //  PostgreSql Database
-            if (connString != null)
-                services.AddDbContext<DirectoryContext>(options => options.UseNpgsql(connString));
+            if (connection != null)
+                services.AddDbContext<DirectoryContext>(options => options.UseNpgsql(connection));
 
             services.AddTransient<IRepository, RepositoryPg>();
 
@@ -66,6 +93,42 @@ namespace DirectoryApi.Server
                 endpoints.MapControllers();
                 endpoints.MapFallbackToFile("index.html");
             });
+        }
+
+        string GetDBPassword()
+        {
+            // Create the client.
+            SecretManagerServiceClient client = SecretManagerServiceClient.Create();
+
+            // Build the resource name.
+            SecretVersionName secretVersionName = new SecretVersionName(
+                                                        "dev-office-294516",
+                                                        "db-password",
+                                                        "1");
+
+            // Call the API.
+            AccessSecretVersionResponse result = client.AccessSecretVersion(secretVersionName);
+
+            // Convert the payload to a string. Payloads are bytes by default.
+            return result.Payload.Data.ToStringUtf8();
+        }
+
+        string GetDBUserName()
+        {
+            // Create the client.
+            SecretManagerServiceClient client = SecretManagerServiceClient.Create();
+
+            // Build the resource name.
+            SecretVersionName secretVersionName = new SecretVersionName(
+                                                        "dev-office-294516",
+                                                        "db-user-name",
+                                                        "1");
+
+            // Call the API.
+            AccessSecretVersionResponse result = client.AccessSecretVersion(secretVersionName);
+
+            // Convert the payload to a string. Payloads are bytes by default.
+            return result.Payload.Data.ToStringUtf8();
         }
     }
 }
